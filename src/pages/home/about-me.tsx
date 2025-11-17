62202d47cfede9ea964451c005e265200a93f39e
Carlos Ian Rodrigues dos Santos <carlosian894@gmail.com>
2025-11-17 11:22:15 -0300

    :sparkles: feat: Adicionado melhoria com o resumo sobre mim

--- src/pages/home/about-me.tsx
+++ src/pages/home/about-me.tsx
@@ -13,14 +13,14 @@ export default function AboutMe() {
           <p className="text-gray-100 text-heading-md md:text-heading-hg">Sobre mim</p>
 
           <p className="md:text-heading-md text-gray-200 max-w-5xl text-heading-xs text-justify leading-7">
-            Sou um Desenvolvedor focado no Back-End, porém já realizei projetos com Front-End.
-            Sempre fui apaixonado pela tecnologia, comecei na área de design gráfico, mas com
-            um tempo fui aprendendo a criar mods para o minecraft. Me adaptei muito nessa área
-            de programação e fiquei muito curioso para saber mais sobre programação de jogos
-            e sites. Em 2020, comecei a estudar no Marwin, uma escola profissionalizante,
-            terminarei a escola em dezembro de 2022. Como eu já sabia programar jogos, então já
-            tinha uma noção de programar sites. Fui e continuo me dedicando e buscando aprender
-            cada vez mais sobre essa área incrível.
+            Sou um desenvolvedor focado em Back-End, mas também já realizei diversos projetos envolvendo Front-End.
+            Sempre fui apaixonado por tecnologia: comecei na área de design gráfico e, com o tempo, passei a criar
+            mods para Minecraft usando Java. Essa experiência despertou meu interesse pela programação e me fez explorar
+            mais sobre o desenvolvimento de jogos e de sites. Em 2020, iniciei meus estudos no Marwin, uma escola
+            profissionalizante, cursando Informática, concluí a formação em dezembro de 2022. Além disso, em 2025,
+            finalizei minha primeira graduação: Tecnólogo em Análise e Desenvolvimento de Sistemas pela Faculdade Uninassau.
+            Como já possuía experiência com programação de jogos, a transição para o desenvolvimento web foi natural.
+            Hoje continuo me dedicando, estudando e buscando evoluir constantemente nessa área incrível que é a tecnologia.
           </p>
         </div>
 
